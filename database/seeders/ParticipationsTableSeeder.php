aa3c8c890d68836db3524cde808801749623604c
Vento <github@brenneisen.me>
2024-03-03 13:08:04 +0100

    Setup seeders

---
+++ database/seeders/ParticipationsTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Faker\Factory;
+use App\Models\Participant;
+use Illuminate\Database\Seeder;
+
+class ParticipationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Factory::create();
+        Participant::create([
+            ['scout_name' => $faker->userName, 'first_name' => $faker->firstName, 'last_name' => $faker->lastName, 'address' => $faker->address, 'plz' => $faker->postcode, 'place' => $faker->city, 'birthday' => $faker->date(), 'gender' => 'male'],
+        ]);
+    }
+}
