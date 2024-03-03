a7263dd28119e38b31dfb976ef18296869f9c1c3
Vento <github@brenneisen.me>
2024-03-03 13:08:18 +0100

    Setup tables

--- database/migrations/2024_02_29_224839_create_points_table.php
+++ database/migrations/2024_02_29_224839_create_points_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('points', function (Blueprint $table) {
             $table->id();
-            $table->string('scout_name');
-            $table->string('first_name');
-            $table->string('last_name');            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('reason')->nullable();
+            $table->integer('points');
+            $table->boolean('is_addition');
+            $table->foreignId('FK_PRT');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('points');
     }
 };
